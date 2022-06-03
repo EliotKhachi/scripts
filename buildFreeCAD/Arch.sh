8c9c0176f7910a7db2c188d9c5226b18ae54fdc3
eliot <eliotkh12@gmail.com>
2022-06-02 18:41:34 -0700

    Re-structured repo for buildFreeCAD scripts

---
+++ buildFreeCAD/Arch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+(( BASH_VERSINFO[0] < 4 )) && echo "Bash 4+ required." && exit 1
+# Build FreeCAD docker container
+cd $REPOS/github.com/eliotkh12/FreeCAD
+git switch mybranch
+cd project-source/tools/build/Arch
+touch xauth_list
+xauth list | sed '2,2d' | sed "s/:/$DISPLAY/" > xauth_list
+docker-compose build
+rm xauth_list
--- buildFreeCAD/Ubuntu-0.20.sh
+++ buildFreeCAD/Ubuntu-0.20.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 (( BASH_VERSINFO[0] < 4 )) && echo "Bash 4+ required." && exit 1
 # Build FreeCAD docker container
-cd $GITLAB_REPOS/eliotkh12/freecad-project/project-source/tools/build/Ubuntu-0.20
+cd $REPOS/github.com/eliotkh12/FreeCAD
+git switch mybranch
+cd project-source/tools/build/Ubuntu-0.20
 touch xauth_list
 xauth list | sed '2,2d' | sed "s/:/$DISPLAY/" > xauth_list
 docker-compose build
