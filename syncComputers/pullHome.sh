dff338b0efb2614b36efbb5eeb852386308ffb5a
eliot <eliotkh12@gmail.com>
2024-03-04 21:55:13 -0800

    Created syncComputers directory. Modified scripts. Added config files

--- syncComputers/pullHome.sh
+++ syncComputers/pullHome.sh
@@ -3,14 +3,14 @@ DISK_MOUNT_PATH="/mnt/backup"
 DISK_DEVICE="/dev/sda"
 
 ## Get latest version of scripts (update snap.txt, exclude.txt, and apt.txt)
-git -C /home/eliot/Public/repos/scripts pull origin
+git -C $REPOS/scripts pull origin
 
 ## Sync directories to backup server. Exclude directories that are not needed.
 sudo rsync -avxz -e "ssh -i /home/$USER/.ssh/rpi" --exclude-from=exclude.txt \
     $PI_USERNAME@$PI_ADDRESS:$DISK_MOUNT_PATH/home/$USER/ /home/$USER/
 
 ## Install snap packages
-xargs -a <(awk '! /^ *(#|$)/' "snap.txt") -r -- sudo snap install ## installs packages from snap.txt, ignoring comments and empty lines
+xargs -a <(awk '! /^ *(#|$)/' "snap.txt") -r -- sudo snap install --classic ## installs packages from snap.txt, ignoring comments and empty lines
  
 ## Install apt packages
 xargs -a <(awk '! /^ *(#|$)/' "apt.txt") -r -- sudo apt-get install ## installs packages from apt.txt, ignoring comments and empty lines
