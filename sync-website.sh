73c7633a9a8edb8523fc2170d8f9b1908af7e4ff
eliot <eliotkh12@gmail.com>
2024-03-04 20:58:14 -0800

    Created syncComputers directory. Modified scripts. Added config files

--- sync-website.sh
+++ sync-website.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 aws s3 sync $REPOS/eliotkhachi.dev s3://amazon-cloudfront-s3-secure-site-cus-s3bucketroot-1jdibk2916043 --exclude=".*"
+aws cloudfront create-invalidation --distribution-id $WEBSITE_CLOUDFRONT_DISTRIBUTION_ID --paths="/*"
